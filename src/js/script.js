e871b41d2eb421b855fe2420645d4e29e4754f3a
eduardo moraes <eduardodmoraes@icloud.com>
2025-05-12 23:56:48 -0300

    Alterações do código

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,20 @@
-let imagem=[
-    'src/assets/imagemcarroslideshow1.jpg',
-    'src/assets/imagenscarroslideshow2.jpg',
-    'src/assets/imagenscarroslideshow3.jpg',
-]
+let imagens = [
+  'src/assets/imagemcarroslideshow1.png',
+  'src/assets/imagemcarroslideshow2.jpg',
+  'src/assets/imagemcarroslideshow3.jpg',
+  'src/assets/imagemcarroslideshow4.jpg'
+];
 
-let i=0;
+let i = 0;
 let tempo = 5000;
 
-function slideshow(){
-    document.getElementById('image').src = imagem[i];
-    i++;
-    if(i == imagem.length){
-        i=0;
-    }
-    setTimeout("slideshow()",tempo)
+function slideShow() {
+  document.getElementById("image").src = imagens[i];
+  i++;
+  if (i >= imagens.length) {
+    i = 0;
+  }
+  setTimeout(slideShow, tempo);
 }
-slideshow();
+
+slideShow();
