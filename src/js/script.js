ae80b822e010fc2d649d2474ec8499c29e6c5216
eduardo moraes <eduardodmoraes@icloud.com>
2025-05-13 21:36:57 -0300

    Projeto estruturado, cards e layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,39 @@
 let imagens = [
-  'src/assets/imagemcarroslideshow1.png',
-  'src/assets/imagemcarroslideshow2.jpg',
-  'src/assets/imagemcarroslideshow3.jpg',
-  'src/assets/imagemcarroslideshow4.jpg'
+  '../src/assets/imagemcarroslideshow1.png',
+  '../src/assets/imagemcarroslideshow2.jpg',
+  '../src/assets/imagemcarroslideshow3.jpg',
+  '../src/assets/imagemcarroslideshow4.jpg'
 ];
 
 let i = 0;
 let tempo = 5000;
 
 function slideShow() {
-  document.getElementById("image").src = imagens[i];
-  i = (i + 1) % imagens.length;
-  setTimeout(slideShow, tempo);
+  const img = document.getElementById("image");
+  if (img) {
+    img.src = imagens[i];
+    i = (i + 1) % imagens.length;
+    setTimeout(slideShow, tempo);
+  }
 }
 
-document.getElementById("esquerda").addEventListener("click", () => {
-  i = (i - 1 + imagens.length) % imagens.length;
-  document.getElementById("image").src = imagens[i];
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const btnEsq = document.getElementById("esquerda");
+  const btnDir = document.getElementById("direita");
 
-document.getElementById("direita").addEventListener("click", () => {
-  i = (i + 1) % imagens.length;
-  document.getElementById("image").src = imagens[i];
-});
+  if (btnEsq && btnDir) {
+    btnEsq.addEventListener("click", () => {
+      i = (i - 1 + imagens.length) % imagens.length;
+      document.getElementById("image").src = imagens[i];
+    });
 
-slideShow();
+    btnDir.addEventListener("click", () => {
+      i = (i + 1) % imagens.length;
+      document.getElementById("image").src = imagens[i];
+    });
+  }
+
+  if (document.getElementById("image")) {
+    slideShow();
+  }
+});
